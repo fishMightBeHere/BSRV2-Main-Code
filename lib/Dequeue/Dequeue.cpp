383a5f8af03183943c3a4cb22cc9b70ec5163467
fishg <fishguy2468@gmail.com>
2023-04-12 12:54:57 -0400

    dequeue skeleton and other edits

---
+++ lib/Dequeue/Dequeue.cpp
@@ -0,0 +1,36 @@
+#include "Dequeue.h"
+
+template<typename T> 
+boolean Dequeue<T>::add(T e) {
+    if (_currentSize)
+}
+
+template<typename T>
+T* Dequeue<T>::peek() {
+
+}
+
+template<typename T>
+T* Dequeue<T>::peekLast() {
+
+}
+
+template<typename T> 
+T* Dequeue<T>::push() {
+
+}
+
+template<typename T>
+T* Dequeue<T>::remove() {
+
+}
+
+template<typename T>
+T* Dequeue<T>::pop() {
+
+}
+
+template<typename T>
+uint16_t Dequeue<T>::size() {
+    
+}
---
+++ lib/Dequeue/Dequeue.h
@@ -0,0 +1,27 @@
+template<typename T> 
+class Dequeue {
+    public:
+        explicit Dequeue(uint16_t maxSize) {
+            _values = (T *) malloc(sizeof(T) * maxSize);
+        }
+        ~Dequeue() {
+            free(_values);
+        }
+
+        boolean add(T e);
+
+        T* peek();        
+        
+        T* peekLast();
+
+        T* push();
+
+        T* remove();
+
+        T* pop();
+
+        uint16_t size();
+
+    protected:
+        T* _values;
+}
