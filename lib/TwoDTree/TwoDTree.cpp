6d48986efed31b8dbc15666e6255e1beda155acf
fishg <fishguy2468@gmail.com>
2023-04-12 14:12:34 -0400

    pointer stuff in tree

--- lib/TwoDTree/TwoDTree.cpp
+++ lib/TwoDTree/TwoDTree.cpp
@@ -1,32 +1,32 @@
 #include "TwoDTree.h"
 
 template<typename T>
-T& TwoDTree<T>::get(int x, int y) {
+T* TwoDTree<T>::get(int x, int y) {
     if (_root == nullptr) {
         return NULL;
     }
 
-    T currentItem = _root;
+    T* currentItem = _root;
     uint16_t k = 0;
     while (true) {
-        if (currentItem.x == x && currentItem.y == y) {
+        if (currentItem->x == x && currentItem->y == y) {
             return currentItem;
         }
 
         if (k%2 == 0) {
-            if (currentItem.leftNode != nullptr && currentItem.x < x) {
-                currentItem = *currentItem.leftNode;
-            } else if (currentItem.right != nullptr && currentItem.x >= x) {
-                currentItem = *currentItem.right;
+            if (currentItem->leftNode != nullptr && currentItem->x < x) {
+                currentItem = currentItem->leftNode;
+            } else if (currentItem->right != nullptr && currentItem->x >= x) {
+                currentItem = currentItem->right;
             }
             else {
                 return NULL;
             }
         } else {
-            if (currentItem.leftNode != nullptr && currentItem.y < y) {
-                currentItem = *currentItem.leftNode;
-            } else if (currentItem.right != nullptr && currentItem.y >= y) {
-                currentItem = *currentItem.right;
+            if (currentItem->leftNode != nullptr && currentItem->y < y) {
+                currentItem = currentItem->leftNode;
+            } else if (currentItem->right != nullptr && currentItem->y >= y) {
+                currentItem = currentItem->right;
             } else {
                 return NULL;
             }
@@ -37,43 +37,43 @@ T& TwoDTree<T>::get(int x, int y) {
 }
 
 template<typename T>
-void TwoDTree<T>::put(T item) {
+void TwoDTree<T>::put(T* item) {
     if (_root == nullptr) {
         _root = item;
     }
 
     boolean k = true;
-    T compareItem = _root;
+    T* compareItem = _root;
     while (true) {
         if (k) {
-            if (comparatorX(item,compareItem) == -1 && compareItem.leftNode != nullptr) {
-                compareItem == *compareItem.leftNode;
+            if (comparatorX(item,compareItem) == -1 && compareItem->leftNode != nullptr) {
+                compareItem = compareItem->leftNode;
             } else {
-                compareItem.leftNode = *item;
+                compareItem->leftNode = item;
                 _values[_putIndex] = item;
                 break;
             }
 
-            if (comparatorX(item, compareItem) != -1 && compareItem.right != nullptr) {
-                compareItem == *compareItem.right;
+            if (comparatorX(item, compareItem) != -1 && compareItem->right != nullptr) {
+                compareItem = compareItem->right;
             } else {
-                compareItem.right = *item;
+                compareItem->right = item;
                 _values[_putIndex] = item;
                 break;
             }
         } else {
-            if (comparatorY(item,compareItem) == -1 && compareItem.leftNode != nullptr) {
-                compareItem == *compareItem.leftNode;
+            if (comparatorY(item,compareItem) == -1 && compareItem->leftNode != nullptr) {
+                compareItem = compareItem->leftNode;
             } else {
-                compareItem.leftNode = *item;
+                compareItem->leftNode = item;
                 _values[_putIndex] = item;
                 break;
             }
 
-            if (comparatorY(item,compareItem) != -1 && compareItem.right != nullptr) {
-                compareItem == *compareItem.right;
+            if (comparatorY(item,compareItem) != -1 && compareItem->right != nullptr) {
+                compareItem = compareItem->right;
             } else {
-                compareItem.right = *item;
+                compareItem->right = item;
                 _values[_putIndex] = item;
                 break;
             }
@@ -91,12 +91,12 @@ boolean TwoDTree<T>::contains(int x, int y) {
 
 template<typename T>
 inline
-int8_t TwoDTree<T>::comparatorX(T i1, T i2) {
-    return i1.x < i2.y ? -1 : 1;
+int8_t TwoDTree<T>::comparatorX(T* i1, T* i2) {
+    return i1->x < i2->y ? -1 : 1;
 }
 
 template<typename T>
 inline
-int8_t TwoDTree<T>::comparatorY(T i1, T i2) {
-    return i1.y < i2.y ? -1 : 1;
+int8_t TwoDTree<T>::comparatorY(T* i1, T* i2) {
+    return i1->y < i2->y ? -1 : 1;
 }
--- lib/TwoDTree/TwoDTree.h
+++ lib/TwoDTree/TwoDTree.h
@@ -11,7 +11,7 @@ class TwoDTree {
             free(_values);
         }
 
-        T& get(int x, int y);
+        T* get(int x, int y);
 
         void put(T item);
 
